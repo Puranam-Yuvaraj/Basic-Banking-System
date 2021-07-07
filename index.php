380de95eb34e3b54f68cc033b163268e818a27e2
Puranam-Yuvaraj <yuvipuranam17@gmail.com>
2021-07-07 20:31:30 +0530

    Commiting a text file

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+  <head>
+      <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <link rel="stylesheet" type="text/css" href="style.css">
+
+    <title>Basic Banking System</title>
+  </head>
+
+  <body background="https://images.unsplash.com/photo-1501167786227-4cba60f6d58f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmFua3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80" style= "background-repeat: no-repeat; background-size: 1540px 753px;">
+            <div class="container">
+</br>
+</br>
+</br>
+</br>
+</br>
+                  <h1> WELCOME TO 'UV' BANK</h1>
+            <div class="activity">
+                    <br>
+                    <a href="tm.php" class="col1">View All Customer's</a>
+                
+                    <a href="th.php" class="col">Transfer Money</a>
+                  
+            </div>
+      </div>
+      <footer class="text">
+        <p>&copy 2021. Made by <b>Yuvaraj</b> </p>
+      </footer>
+      </body>
+</html>
